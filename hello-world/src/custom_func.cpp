ec484deaad1ff63939dcbdbbb5e34a614687c0a4
Zhiquan Wang <zhiquan.wzq@gmail.com>
2020-02-17 21:37:16 -0500

    camera done

--- hello-world/src/custom_func.cpp
+++ hello-world/src/custom_func.cpp
@@ -5,10 +5,49 @@
 // Copyright (c) 2020 Zhiquan Wang. All rights reserved.
 //
 #include "ZWEngine.h"
+
 //#define STB_IMAGE_IMPLEMENTATION
 //
 //#include "stb_image.h"
+static ZWEngine *self;
 
+void framebuffer_size_callback(GLFWwindow *window, int w, int h) {
+    glViewport(0, 0, w, h);
+}
+
+bool first_in = true;
+glm::vec2 pre_mouse_pos;
+// callback function
+
+
+void ZWEngine::keycode_callback(GLFWwindow *window, int key, int scancode, int action, int mode) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        self->main_camera.process_keyboard(FORWARD, self->delta_time);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        self->main_camera.process_keyboard(BACKWARD, self->delta_time);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        self->main_camera.process_keyboard(LEFT, self->delta_time);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        self->main_camera.process_keyboard(RIGHT, self->delta_time);
+}
+
+
+void ZWEngine::mouse_callback(GLFWwindow *window, double xpos, double ypos) {
+    if (first_in) {
+        pre_mouse_pos = glm::vec2(xpos, ypos);
+        first_in = false;
+    }
+    glm::vec2 offset(xpos - pre_mouse_pos.x, pre_mouse_pos.y - ypos);
+    pre_mouse_pos = glm::vec2(xpos, ypos);
+    self->main_camera.process_mouse_movement(offset);
+}
+
+void ZWEngine::scroll_callback(GLFWwindow *window, double xoffset, double yoffset) {
+    self->main_camera.process_mouse_scroll(yoffset);
+}
 
 // Opengl functions
 void ZWEngine::set_render_objects() {
@@ -26,8 +65,11 @@ void ZWEngine::set_render_objects() {
     VertexArrayObject vao(true);
     VertexBufferObject vbo(vertices, GL_STATIC_DRAW);
     ElementBufferObject ebo(indices, GL_STATIC_DRAW);
+    //pos
     bind_vertex_attribute(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void *) nullptr);
+    //col
     bind_vertex_attribute(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void *) (3 * sizeof(GLfloat)));
+    //texture coord
     bind_vertex_attribute(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void *) (6 * sizeof(GLfloat)));
     vao.attach_vbo(vbo.id());
     vao.attach_ebo(ebo.id());
@@ -41,7 +83,10 @@ void ZWEngine::set_render_objects() {
     this->add_texture(tex_0);
     this->add_texture(tex_1);
 }
+
 void ZWEngine::draw_frame() {
+
+    // clear buffers
     glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 //    glActiveTexture(GL_TEXTURE0);
@@ -51,7 +96,7 @@ void ZWEngine::draw_frame() {
     this->activate_texture();
     this->activate_vao("tmp_vao");
 //    glDrawArrays(GL_TRIANGLES,0,3);
-    glDrawElements(GL_TRIANGLES, 6,GL_UNSIGNED_INT,0);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
 void ZWEngine::process_input(GLFWwindow *window) {
