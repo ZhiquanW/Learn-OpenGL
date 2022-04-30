facb1051a83e726a5e13dcbe2d4b47d3ae61dba1
Zhiquan <zhiquan.wzq@gmail.com>
2022-04-30 19:31:30 -0400

    [xps:dev] add dependency

--- HelloWorld/src/main.cpp
+++ HelloWorld/src/main.cpp
@@ -1,7 +1,7 @@
 
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+// #include "imgui.h"
+// #include "imgui_impl_glfw.h"
+// #include "imgui_impl_opengl3.h"
 
 #include <glad/gl.h>
 #define GLFW_INCLUDE_NONE
